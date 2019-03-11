ee26706a2890616e1ff3df4c786db335edf69e1a
walshcommaryan <rywalsh@uci.edu>
2019-03-10 18:16:31 -0700

    testing

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler hi
 app.use(function(req, res, next) {
   next(createError(404));
 });
